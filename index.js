219506906520d4703ff53a80369d7020eb4ef4f8
yogesh-hack <yogesh0909baghel@gmail.com>
2023-04-04 16:07:44 +0530

    adding files

---
+++ index.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+const TextEditor = () => {
+  const [bold, setBold] = useState(false);
+  const [italic, setItalic] = useState(false);
+  const [underline, setUnderline] = useState(false);
+  const [text, setText] = useState("");
+
+  const handleBoldClick = () => {
+    setBold(!bold);
+  };
+
+  const handleItalicClick = () => {
+    setItalic(!italic);
+  };
+
+  const handleUnderlineClick = () => {
+    setUnderline(!underline);
+  };
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={handleBoldClick} style={{ fontWeight: bold ? "bold" : "normal" }}>
+          B
+        </button>
+        <button onClick={handleItalicClick} style={{ fontStyle: italic ? "italic" : "normal" }}>
+          I
+        </button>
+        <button onClick={handleUnderlineClick} style={{ textDecoration: underline ? "underline" : "none" }}>
+          U
+        </button>
+      </div>
+      <textarea value={text} onChange={handleTextChange} />
+    </div>
+  );
+};
+
+export default TextEditor;
